bd9c0434c7796e93e0c5e14da8f6692bf44cf390
Shahabuddin Ahmad <meetshahab2@gmail.com>
2025-09-18 16:39:04 +0530

    pull update

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,4 +1,5 @@
 const CryptoJS = require("crypto-js");
+const CryptoJS = require("crypto-js");
 
 function secureEncrypt(payload, key) {
     const ivBytes = CryptoJS.lib.WordArray.random(16);
@@ -24,20 +25,102 @@ function secureEncrypt(payload, key) {
     return response;
 }
 
-function renderNotFound(res) {
-    return res.status(404).render("404", {
-        title: "Oops! Nothing here",
-        page: "404",
-        description: "We couldn’t find the page you were looking for.",
-        source: "/",
-        keywords: "error, missing, not found"
-    });
+function isDateGreaterThanOrEqualToTodayInIST(date) {
+    // Create a Moment object for the current date in IST
+    const currentDateIST = moment().tz('Asia/Kolkata').startOf('day');
+
+    // Create a Moment object for the input date
+    const inputDate = moment(date, dateFormat).tz('Asia/Kolkata').startOf('day');
+
+    // Compare the input date with the current date
+    return inputDate.isSameOrAfter(currentDateIST);
 }
 
-module.exports = {
-    secureEncrypt,
-    renderNotFound
-};
 
 
 
+function isThursday(day) {
+    return day === 4;
+}
+
+function indexSymbol() {
+    return ["NIFTY", "BANKNIFTY", "MIDCPNIFTY", "FINNIFTY", "NIFTYNXT50", "SENSEX", "BANKEX"];
+
+}
+
+function isIndexSymbol(symbol) {
+    return indexSymbol().includes(symbol?.toUpperCase());
+}
+
+function isCurrencySymbol(symbol) {
+    symbol = symbol.toUpperCase();
+    return symbol === "USDINR";
+}
+function isCommoditySymbol(symbol) {
+    symbol = symbol.toUpperCase();
+    return symbol === "NATURALGAS" ||
+        symbol === "CRUDEOIL" || symbol === "GOLD"
+}
+
+function isCurrOrCommoditySymbol(symbol) {
+    symbol = symbol.toUpperCase();
+    return symbol === "USDINR" || symbol === "NATURALGAS" ||
+        symbol === "CRUDEOIL" || symbol === "GOLD"
+}
+
+function generateCacheKeys(cacheName, symbol, expiry) {
+    let cacheKey = `${cacheName}_${isIndexSymbol(symbol) ? `INDEX` : expiry}`;
+    let primaryKey = `${isIndexSymbol(symbol) ? `${symbol}_${expiry}` : `${symbol}`}`;
+    return { cacheKey, primaryKey };
+}
+
+
+function getClassNameForPCR(val) {
+    if (val > 0 && val <= 0.8) {
+        return "danger"
+    } else if (val >= 1.2) {
+        return "success"
+    } else if (val == 0) {
+        return "secondary"
+    } else return "info"
+}
+
+function getTextForPCR(val) {
+    if (val > 0 && val <= 0.8) {
+        return "Bearish Trend"
+    } else if (val >= 1.2) {
+        return "Bullish Trend"
+    } else if (val == 0) {
+        return "InSufficient Data"
+    } else return "Neutral Trend"
+}
+
+function capitalizeFirstLetterOfEachWord(name) {
+    return name
+        ?.toLowerCase() // Convert the entire string to lowercase
+        ?.replace(/\b\w/g, char => char.toUpperCase()) ?? ""; // Capitalize the first letter of each word
+}
+
+/*
+module.exports.getTextForPCR = getTextForPCR;
+module.exports.getClassNameForPCR = getClassNameForPCR;
+module.exports.notFoundPage = notFoundPage;
+module.exports.redirectTo404 = redirectTo404;
+module.exports.redirectOnException = redirectOnException;
+module.exports.groupByKey = groupByKey;
+module.exports.tickerFormatting = tickerFormatting;
+module.exports.capitalizeWords = capitalizeWords;
+module.exports.showLiveTickerFor = showLiveTicker;
+module.exports.yesterday = yesterday;
+module.exports.isThursday = isThursday;
+module.exports.isIndexSymbol = isIndexSymbol;
+module.exports.isCurrOrCommoditySymbol = isCurrOrCommoditySymbol;
+module.exports.isCurrencySymbol = isCurrencySymbol;
+module.exports.generateCacheKeys = generateCacheKeys;
+module.exports.seoHeadTagValues = seoHeadTagValues;
+module.exports.todayIST = todayIST;
+module.exports.indexSymbol = indexSymbol;
+module.exports.isDateGreaterThanOrEqualToTodayInIST = isDateGreaterThanOrEqualToTodayInIST;
+module.exports.capitalizeFirstLetterOfEachWord = capitalizeFirstLetterOfEachWord;
+module.exports.encryptData = encryptData;
+*/
